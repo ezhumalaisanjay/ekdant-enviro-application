ebf33648245b7e62f1cbdc72615aa288bc000e42
ezhumalaisanjay <ezhumalaisanjay05@gmail.com>
2025-01-20 16:37:03 +0530

    add

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,13 @@
 "use client"
 import ServiceRequest from "../app/Pages/ServiceRequest"
-import Dashboard from "../app/Pages/DashBoard"
 
 export default function Layout() {
   
   return (
     <>
-      <div> 
+  
         <ServiceRequest />
-      </div>
+     
     </>
   );
 }
