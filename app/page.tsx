6be7b8a5affa12a84226bcb3a9d583b810873d6e
ezhumalaisanjay <ezhumalaisanjay05@gmail.com>
2025-01-20 08:36:53 +0530

    add

--- app/page.tsx
+++ app/page.tsx
@@ -1,25 +1,42 @@
-"use client"
-import { SidebarProvider, SidebarTrigger } from "@/components/ui/sidebar";
-import { AppSidebar } from "./app-sidebar/AppSidebar";
-import { ResizableHandle, ResizablePanel, ResizablePanelGroup } from "@/components/ui/resizable";
-import HeaderSection from "./Header/header"
-import { useEffect, useState } from "react";
-import { MobileSidebar } from "./app-sidebar/AppSidebar";
-import ServiceRequest from "./Form/ServiceRequest"
+import { AppSidebar } from "@/components/app-sidebar"
+import {
+  Breadcrumb,
+  BreadcrumbItem,
+  BreadcrumbLink,
+  BreadcrumbList,
+  BreadcrumbPage,
+  BreadcrumbSeparator,
+} from "@/components/ui/breadcrumb"
+import { Separator } from "@/components/ui/separator"
+import { SidebarInset, SidebarProvider, SidebarTrigger } from "@/components/ui/sidebar"
 
-export default function Layout() {
+export default function Page() {
   return (
-    <div className="flex">
-      <SidebarProvider> 
-        <AppSidebar /> 
-        <main className="w-full"> 
-          <SidebarTrigger />
-          <div>
-            <ServiceRequest />
-          </div>  
-        </main> 
-        </SidebarProvider>
-    </div>
-  );
+    <SidebarProvider>
+      <AppSidebar />
+      <SidebarInset>
+        <header className="flex h-16 shrink-0 items-center gap-2 transition-[width,height] ease-linear group-has-[[data-collapsible=icon]]/sidebar-wrapper:h-12">
+          <div className="flex items-center gap-2 px-4">
+            <SidebarTrigger className="-ml-1" />
+            <Separator orientation="vertical" className="mr-2 h-4" />
+            <Breadcrumb>
+              <BreadcrumbList>
+                <BreadcrumbItem className="hidden md:block">
+                  <BreadcrumbLink href="#">Building Your Application</BreadcrumbLink>
+                </BreadcrumbItem>
+                <BreadcrumbSeparator className="hidden md:block" />
+                <BreadcrumbItem>
+                  <BreadcrumbPage>Data Fetching</BreadcrumbPage>
+                </BreadcrumbItem>
+              </BreadcrumbList>
+            </Breadcrumb>
+          </div>
+        </header>
+        <div className="flex flex-1 flex-col gap-4 p-4 pt-0">
+       
+        </div>
+      </SidebarInset>
+    </SidebarProvider>
+  )
 }
 
