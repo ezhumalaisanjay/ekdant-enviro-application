7aeb4bc8c132fdaaf21e4483aba272c3fce8e545
ezhumalaisanjay <ezhumalaisanjay05@gmail.com>
2025-02-03 12:43:55 +0530

    add

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -10,9 +10,15 @@ import {
 } from "@/components/ui/card";
 import { Input } from "@/components/ui/input";
 import { Label } from "@radix-ui/react-dropdown-menu";
-import { signUp, confirmSignUp, signIn } from "@/lib/cognito"; // Import sign-in function
+import {
+  signUp,
+  confirmSignUp,
+  signIn,
+  forgotPassword,
+  resetPassword,
+} from "@/lib/cognito"; // Import Cognito functions
 import Image from "next/image";
-import logo from "../../imges/ekdant-logo-icon.png"
+import logo from "../../imges/ekdant-logo-icon.png";
 import { Eye, EyeClosed, Loader } from "lucide-react";
 
 function Login() {
@@ -24,72 +30,99 @@ function Login() {
   const [isLoading, setIsLoading] = useState(false);
   const [confirmationCode, setConfirmationCode] = useState("");
   const [signUpSuccess, setSignUpSuccess] = useState(false);
-  const [errorMessage, setErrorMessage] = useState(""); // Store error messages
+  const [errorMessage, setErrorMessage] = useState("");
   const [isPasswordVisible, setIsPasswordVisible] = useState(false);
+  const [forgotPasswordMode, setForgotPasswordMode] = useState(false); // State for forgot password mode
+  const [newPassword, setNewPassword] = useState(""); // New password for reset
+  const [resetConfirmationCode, setResetConfirmationCode] = useState(""); // Reset code
 
   const togglePasswordVisibility = () => {
     setIsPasswordVisible(!isPasswordVisible);
   };
 
-  // Handle user sign-up
+  // Handle sign-up
   const handleSignUp = async () => {
     setIsLoading(true);
-    setErrorMessage(""); // Clear errors
+    setErrorMessage("");
     try {
       await signUp(email, password, fullName, phoneNumber);
       setSignUpSuccess(true);
-    } catch (error: unknown) {
-      if (error instanceof Error) {
-        setErrorMessage(error.message || "Sign-in failed. Check your credentials.");
-      } else {
-        setErrorMessage("Sign-in failed. Check your credentials.");
-      }
+    } catch (error) {
+      setErrorMessage(error.message || "Sign-up failed. Try again.");
     }
     setIsLoading(false);
   };
 
-  // Handle confirmation code submission
+  // Handle confirmation code for sign-up
   const handleConfirmCode = async () => {
     setIsLoading(true);
     setErrorMessage("");
     try {
       await confirmSignUp(fullName, confirmationCode);
-    } catch (error: unknown) {
-      if (error instanceof Error) {
-        setErrorMessage(error.message || "Sign-in failed. Check your credentials.");
-      } else {
-        setErrorMessage("Sign-in failed. Check your credentials.");
-      }
+    } catch (error) {
+      setErrorMessage(error.message || "Confirmation failed.");
     }
     setIsLoading(false);
   };
 
-  // Handle user sign-in
+  // Handle sign-in
   const handleSignIn = async () => {
     setIsLoading(true);
     setErrorMessage("");
     try {
       await signIn(email, password);
-    } catch (error: unknown) {
-      if (error instanceof Error) {
-        setErrorMessage(error.message || "Sign-in failed. Check your credentials.");
-      } else {
-        setErrorMessage("Sign-in failed. Check your credentials.");
-      }
+    } catch (error) {
+      setErrorMessage(
+        error.message || "Sign-in failed. Check your credentials."
+      );
+    }
+    setIsLoading(false);
+  };
+
+  // Handle forgot password (send reset code)
+  const handleForgotPassword = async () => {
+    setIsLoading(true);
+    setErrorMessage("");
+    try {
+      await forgotPassword(email); // Send reset code to email
+      setForgotPasswordMode(true); // Switch to password reset mode
+    } catch (error) {
+      setErrorMessage(error.message || "Password reset failed.");
+    }
+    setIsLoading(false);
+  };
+
+  // Handle password reset
+  const handleResetPassword = async () => {
+    setIsLoading(true);
+    setErrorMessage("");
+    try {
+      await resetPassword(email, resetConfirmationCode, newPassword); // Reset password with new password and reset code
+      setForgotPasswordMode(false); // Exit reset password mode
+    } catch (error) {
+      setErrorMessage(error.message || "Password reset failed.");
     }
     setIsLoading(false);
   };
 
   return (
     <div className="flex h-[710px] justify-center items-center">
-      {!signUpMode ? (
-        // SIGN-IN FORM
+      {/* Show Sign In form if forgotPasswordMode is false */}
+      {!forgotPasswordMode && !signUpMode ? (
         <Card className="md:w-[460px] p-10 border border-slate-400 border-opacity-25">
           <CardHeader className="pb-2">
             <div className="text-center">
               <div className="flex flex-col justify-center items-center gap-3 p-3">
-                <Image src={logo} className="items-center" alt="Logo" width={50} height={50}/>
-                <div className="text-xs text-green-600 font-semibold">Ekdant Enviro Services Ltd</div>
+                <Image
+                  src={logo}
+                  className="items-center"
+                  alt="Logo"
+                  width={50}
+                  height={50}
+                />
+                <div className="text-xs text-green-600 font-semibold">
+                  Ekdant Enviro Services Ltd
+                </div>
               </div>
               <p className="text-lg font-semibold">Sign In</p>
             </div>
@@ -102,7 +135,7 @@ function Login() {
               <div>
                 <Label>Username/Email</Label>
                 <Input
-                  placeholder="username or Email"
+                  placeholder="Username or Email"
                   className="p-5"
                   value={email}
                   onChange={(e) => setEmail(e.target.value)}
@@ -121,11 +154,24 @@ function Login() {
                   className="absolute right-3 top-11 transform -translate-y-1/2 cursor-pointer"
                   onClick={togglePasswordVisibility}
                 >
-                  {isPasswordVisible ? <Eye className="size-5" /> : <EyeClosed className="size-5"  />}
+                  {isPasswordVisible ? (
+                    <Eye className="size-5" />
+                  ) : (
+                    <EyeClosed className="size-5" />
+                  )}
                 </span>
               </div>
               <div className="flex justify-end">
-                <a href="" className="text-sm text-blue-500">Forgot Password?</a>
+                <a
+                  href="#"
+                  className="text-sm text-blue-500"
+                  onClick={(e) => {
+                    e.preventDefault();
+                    handleForgotPassword();
+                  }} // Trigger forgot password
+                >
+                  Forgot Password?
+                </a>
               </div>
             </div>
             {errorMessage && (
@@ -137,23 +183,64 @@ function Login() {
                 onClick={handleSignIn}
                 disabled={isLoading}
               >
-                {isLoading ? <>  <Loader className="animate-spin" /> Logging in </> : "Log in"}
+                {isLoading ? (
+                  <>
+                    {" "}
+                    <Loader className="animate-spin" /> Logging in{" "}
+                  </>
+                ) : (
+                  "Log in"
+                )}
               </Button>
             </div>
-            {/*
-            <div className="text-center font-light text-sm mt-4">
-              Don&rsquo;t have an account?{" "}
-              <span
-                className="font-semibold cursor-pointer"
-                onClick={() => setSignUpMode(true)}
-              >
-                Sign up
-              </span>
-            </div> */}
           </CardContent>
         </Card>
-      ) : (
-        // SIGN-UP FORM
+      ) : null}
+
+      {/* Show Forgot Password form when forgotPasswordMode is true */}
+      {forgotPasswordMode && (
+        <Card className="md:w-[460px] p-10 border border-slate-400 border-opacity-25">
+          <CardHeader className="pb-2">
+            <p className="text-lg font-semibold">Reset Password</p>
+          </CardHeader>
+          <CardContent className="mt-8">
+            <div className="flex flex-col gap-3">
+              <div>
+                <Label>Enter Reset Code</Label>
+                <Input
+                  placeholder="Reset Code"
+                  className="p-5 pl-1"
+                  value={resetConfirmationCode}
+                  onChange={(e) => setResetConfirmationCode(e.target.value)}
+                />
+              </div>
+              <div>
+                <Label>New Password</Label>
+                <Input
+                  type="password"
+                  placeholder="New Password"
+                  className="p-5 pl-1"
+                  value={newPassword}
+                  onChange={(e) => setNewPassword(e.target.value)}
+                />
+              </div>
+            </div>
+            {errorMessage && (
+              <p className="text-red-500 mt-2">{errorMessage}</p>
+            )}
+            <Button
+              className="flex w-full mt-4"
+              onClick={handleResetPassword}
+              disabled={isLoading}
+            >
+              {isLoading ? "Loading..." : "Reset Password"}
+            </Button>
+          </CardContent>
+        </Card>
+      )}
+
+      {/* Sign-up form, if user is in sign-up mode */}
+      {signUpMode && !forgotPasswordMode && (
         <Card className="md:w-[460px] p-10 border border-slate-400 border-opacity-25">
           <CardHeader className="pb-2">
             <div className="flex flex-col justify-center text-center">
@@ -219,33 +306,8 @@ function Login() {
                   Sign-up successful! Please check your email for the
                   confirmation code.
                 </p>
-                <div className="mt-4">
-                  <Label>Enter Confirmation Code</Label>
-                  <Input
-                    placeholder="Confirmation Code"
-                    className="p-5 pl-1"
-                    value={confirmationCode}
-                    onChange={(e) => setConfirmationCode(e.target.value)}
-                  />
-                </div>
-                <Button
-                  className="flex w-full mt-4"
-                  onClick={handleConfirmCode}
-                  disabled={isLoading}
-                >
-                  {isLoading ? "Loading..." : "Confirm Code"}
-                </Button>
               </div>
             )}
-            <div className="text-center font-light text-sm mt-4">
-              Already have an account?{" "}
-              <span
-                className="font-semibold cursor-pointer"
-                onClick={() => setSignUpMode(false)}
-              >
-                Log in
-              </span>
-            </div>
           </CardContent>
         </Card>
       )}
